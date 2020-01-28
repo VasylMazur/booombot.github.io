7c4e72bc08c19ce8a54e96fb208e20d8cc16b316
RkVishko <vasa8061@gmail.com>
2020-01-28 14:01:44 +0200

    fix bugs

---
+++ precache-manifest.1487107607ba1f9b4d9ce759d6301bdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5badf911fe01c0f97c39af052b0603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10bd85f9dc72f58a2601",
+    "url": "/static/css/main.a50eb33f.chunk.css"
+  },
+  {
+    "revision": "14d4586c0a13371e26da",
+    "url": "/static/js/2.da676d8e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.da676d8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10bd85f9dc72f58a2601",
+    "url": "/static/js/main.27a45af9.chunk.js"
+  },
+  {
+    "revision": "f782ed4cd47f48e522f5",
+    "url": "/static/js/runtime-main.4f810aa5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lynqdemo/precache-manifest.1a953ab7400fdb1579038c9b88ba1857.js"
+  "/precache-manifest.1487107607ba1f9b4d9ce759d6301bdd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lynqdemo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
